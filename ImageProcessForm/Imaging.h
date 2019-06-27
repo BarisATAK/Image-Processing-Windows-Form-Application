30060d96812e9c19757c8a8679063b47d09442a2
BarisAtak <39070206+BarisATAK@users.noreply.github.com>
2019-06-27 23:31:37 +0300

    Update Imaging.h

--- ImageProcessForm/Imaging.h
+++ ImageProcessForm/Imaging.h
@@ -2,9 +2,11 @@
 #include <windows.h>
 BYTE* Frame(BYTE* image, int w, int h);
 BYTE* Gradient_Magnitude(BYTE* vertical_image, BYTE* horizontal_image, int w, int h);
-BYTE* Non_Max_Suppression(BYTE* gradient_magnitude, BYTE* vertical_image, BYTE* horizontal_image, int w, int h);
+double* Edge_Degrees(BYTE* vertical_image, BYTE* horizontal_image, int w, int h);
+BYTE* Non_Max_Suppression(BYTE* gradient_magnitude,double* edge_degrees, int w, int h);
 BYTE* Double_Threshold(BYTE* n_m_suppression, int w, int h);
 BYTE* Canny_Image(BYTE* double_threshold, int w, int h);
+BYTE* Hough_Transform(BYTE* canny_image, double* edge_degrees, int w, int h);
 BYTE* Labeling(BYTE* image, int w, int h);
 BYTE* K_Means(BYTE* image, int* hist, int w, int h);
 BYTE* Dilation(BYTE* image, int w, int h);
